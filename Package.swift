972cf980759537d21174af3e6ec8254da7a48ffb
Christoph Hagen <github@christophhagen.de>
2024-04-13 11:27:32 +0200

    Add DocC dependency for documentation

--- Package.swift
+++ Package.swift
@@ -13,6 +13,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/christophhagen/CBORCoding", from: "1.0.0"),
         .package(url: "https://github.com/christophhagen/Clairvoyant", from: "0.7.0"),
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
     ],
     targets: [
         .target(
